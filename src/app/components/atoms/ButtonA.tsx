6db0aa86aa3690a45b503f01d920a4cddf1e40a6
Ulises <ulises.develop@gmail.com>
2025-05-09 19:16:05 -0600

    Authjs configured

--- src/app/components/atoms/ButtonA.tsx
+++
@@ -1,5 +0,0 @@
-import { FC, ButtonHTMLAttributes } from "react";
-
-export const ButtonA: FC<ButtonHTMLAttributes<HTMLButtonElement>> = (props) => {
-    return <button {...props} className="h-10 w-40 bg-sky-900 hover:bg-sky-500 text-white rounded-md cursor-pointer" />
-}
