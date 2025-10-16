18ee34418b2420a1cceb37d9b3db561f9e449f97
Ulises <ulises.develop@gmail.com>
2025-10-16 13:00:04 -0600

    Fixed ButtonC props

--- src/components/atoms/ButtonC.tsx
+++ src/components/atoms/ButtonC.tsx
@@ -3,5 +3,6 @@ import './css/Button.css'
 import './css/ButtonC.css'
 
 export const ButtonC: FC<ButtonHTMLAttributes<HTMLButtonElement> & { active: boolean }> = (props) => {
-  return <button {...props} className={`buttonC ${props.className} ${props.active && 'buttonC-active'}`} />
+  const { active, ...restProps } = props
+  return <button {...restProps} className={`buttonC ${props.className} ${active ? 'buttonC-active' : ''}`} />
 }
