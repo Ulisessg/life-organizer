767dbc6c340ee7c21b6e85297cba72a936c95d3a
Ulises <ulises.develop@gmail.com>
2025-11-05 14:05:26 -0600

    Add ingredient to personal larder, button disabled when form is not properly fulfilled

--- src/components/molecules/AddIngredientToPersonalLarder.tsx
+++ src/components/molecules/AddIngredientToPersonalLarder.tsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "@/redux/store";
-import { useId, useMemo } from "react";
+import { useEffect, useId, useMemo, useState } from "react";
 import './css/AddIngredientToLarder.css'
 import { ButtonA } from "../atoms/ButtonA";
 import { addIngredientInUserLarderSchema } from "@/schemas/ingredientInUserLarderSchema";
@@ -10,6 +10,11 @@ export function AddIngredientToPersonalLarder() {
   const personalIngredientsInState = useSelector((state: RootState) => state.userIngredients)
   const unitsOfMeasure = useSelector((state: RootState) => state.unitsOfMeasure)
   const userIngredientsInLarder = useSelector((state: RootState) => state.userLarderIngredients)
+  const [ingredientSelected, setIngredientSelected] = useState<string>("default")
+  const [unitOfMeasureSelected, setUnitOfMeasureSelected] = useState<string>("default")
+  const [ingredientQty, setIngredientQty] = useState<string>("")
+  const [expirationDate, setExpirationDate] = useState<string>("")
+  const [disableSubmitButton, setDisableSubmitButton] = useState<boolean>(false)
   const userIngredientsInLarderIDS = useMemo(() => {
     const ingredientsIds: Record<number, boolean> = {}
     userIngredientsInLarder.forEach((ingredientInLarder) => {
@@ -67,27 +72,42 @@ export function AddIngredientToPersonalLarder() {
     })
   }
 
+  useEffect(() => {
+    const {success, error}  = addIngredientInUserLarderSchema.safeParse({
+      user_ingredient_id: Number(ingredientSelected),
+      unit_of_measure_id: Number(unitOfMeasureSelected),
+      quantity: Number(ingredientQty),
+      expiration_date: expirationDate || undefined
+    })
+    console.log(error)
+    if(success) {
+      setDisableSubmitButton(false)
+    } else {
+      setDisableSubmitButton(true)
+    }
+  }, [ingredientSelected,unitOfMeasureSelected,ingredientQty, expirationDate])
+
   return <section className="add-ingredient-to-larder section_container">
     <h3>Agrega un ingrediente a la alacena personal</h3>
     <form>
       <label htmlFor={selectIngredientID}>Selecciona el ingrediente</label>
-      <select name="" id={selectIngredientID} defaultValue="default" >
+      <select id={selectIngredientID} value={ingredientSelected} onChange={(e) => setIngredientSelected(e.currentTarget.value)} >
         <option value="default" disabled>Elige</option>
         {personalIngredients.map((ingredient) => {
           return <option key={ingredient.id} value={ingredient.id}>{ingredient.name}</option>
         })}
       </select>
       <label htmlFor={selectUOMID}>Selecciona la unidad de medida</label>
-      <select name="" id={selectUOMID} defaultValue="default" >
+      <select name="" id={selectUOMID} value={unitOfMeasureSelected} onChange={(e) => setUnitOfMeasureSelected(e.currentTarget.value)} >
         <option value="default" disabled>Elige</option>
         {unitsOfMeasure.map((unitOfMeasure) =>
           <option key={`${unitOfMeasure.id}${unitOfMeasure.name}`} value={unitOfMeasure.id}>{unitOfMeasure.name}</option>)}
       </select>
       <label htmlFor={ingredientQTYID}>Ingresa la cantidad del ingrediente</label>
-      <input id={ingredientQTYID} type="number" min="1" />
+      <input id={ingredientQTYID} value={ingredientQty} onChange={(e) => setIngredientQty(e.currentTarget.value)} type="number" min="1" />
       <label htmlFor={expirationDateID}>Fecha de expiración</label>
-      <input id={expirationDateID} type="date" />
-      <ButtonA type="button" onClick={addIngredient}>Agregar ingrediente a la alacena</ButtonA>
+      <input id={expirationDateID} type="date" value={expirationDate} onChange={(e) => setExpirationDate(e.currentTarget.value)} />
+      <ButtonA type="button" onClick={addIngredient} disabled={disableSubmitButton}>Agregar ingrediente a la alacena</ButtonA>
     </form>
   </section>
 }
